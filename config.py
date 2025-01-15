74c5e8e48552e86a52b7894cc4002a7707eb544e
TestSub747 <aurigacosmic430@gmail.com>
2025-01-15 14:39:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ BOT_STATS_TEXT = os.environ.get("BOTS_STATS_TEXT","<b>BOT UPTIME </b>\n{uptime}"
 USER_REPLY_TEXT = os.environ.get("USER_REPLY_TEXT", "ONLY ADMIN CAN ACCESS ! <b>Want To Create Your Private File Share Bot ? Contact @hf_owner ! CHANNEL - @hacking_freak</b>")
 
 #your bot token here from https://telegram.me/BotFather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7876492066:AAEI6JQDus-1Cbzctl87mucuBh3qtu5nWm4") 
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "") 
 #your api id from https://my.telegram.org/apps
 APP_ID = int(os.environ.get("APP_ID", "23630387"))
 #your api hash from https://my.telegram.org/apps
